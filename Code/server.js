14797a1c64379bb3e3958b513eb727ece6fc8e65
NathanTan <nathantan97@gmail.com>
2019-01-09 17:03:24 -0800

    Set up for server sending the inital index file with css.

---
+++ Code/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+let path = require('path')
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
