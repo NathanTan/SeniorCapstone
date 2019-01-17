eb2b906f197329fe52e7bc4ab85e43468950ec73
Mattop <mattop@10-249-209-138.wireless.oregonstate.edu>
2019-01-16 17:25:34 -0800

    added basic fetch framework

--- Code/server.js
+++ Code/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
+const fetch = require('node-fetch')
 let path = require('path')
-
+let flightVars = "Good Stuff"
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
@@ -8,6 +9,10 @@ app.use(express.static(__dirname + '/src'))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/flightVars', function(req, res) {
+    res.send(JSON.stringify(flightVars))
 })
 
 const port = process.env.PORT || 3000
