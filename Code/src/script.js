f98ea7f4506896c4656a263976afa881d3706797
Mattop <mattop@10-249-209-138.wireless.oregonstate.edu>
2019-01-23 17:20:53 -0800

    added flightVars object to client side. did a quick fuctionality test. looks good.

--- Code/src/script.js
+++ Code/src/script.js
@@ -1,9 +1,24 @@
+//Jan 23: not sure how the data will look when piped off MAV. Best guess for now, feel free to change.
+//Jan 23: X is forward/back, Y is SidetoSide, Z is verticle. Is there a standard in flight?
+//Jan 23: for sonic sensors, naming orientation is as if you were a pilot in a helicopter
+//Jan 23: trying to get everything, even things we might not need 
+//Jan 23: Set all vars to 1 to test that object is updated correctly after fetch
+let flightVars = {speedMag:1, speedX:1, speedY:1, speedZ:1, accelX:1, accelY:1, accelZ:1, pitchChange:1, rollChange:1,
+  orientationPitch:1, orientationRoll:1, height:1, ledsON:1, speakerON:1}
+
+  //Jan 23: test to make sure local flightVars object is updated correctly after fetch
+console.log(flightVars);
 fetch('/flightVars')
     .then(function (response) {
         return response.json();
     })
     .then(function (myJson) {
+        flightVars=myJson;
+        return myJson;
+    })
+    .then(function (myJson) {
         console.log(JSON.stringify(myJson));
+        console.log(flightVars);
     });
 
 //For switching between the depth map and bottom camera feed
